27581ddf12e5ce4212203fa9fec3d8b5e6debfe6
freddehnashi <freddehnashi@gmail.com>
2026-02-23 18:49:19 -0500

    Replace browser alerts with global in-app toast notifications

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App';
+import { installToastAlerts } from './utils/installToastAlerts';
 import './index.css';
+
+installToastAlerts();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

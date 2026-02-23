65200abd4aae375656df2dc6c33f380678a40418
freddehnashi <freddehnashi@gmail.com>
2026-02-23 15:25:08 -0500

    Split vendor bundle into smaller cacheable chunks

--- vite.config.js
+++ vite.config.js
@@ -66,8 +66,53 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
+          if (!id.includes('node_modules')) {
+            if (id.includes('components')) return 'components';
+            return;
+          }
+
+          if (id.includes('/node_modules/react/') || id.includes('/node_modules/react-dom/')) {
+            return 'vendor-react';
+          }
+          if (id.includes('/node_modules/react-router/') || id.includes('/node_modules/react-router-dom/')) {
+            return 'vendor-router';
+          }
+          if (
+            id.includes('/node_modules/@mui/') ||
+            id.includes('/node_modules/@emotion/')
+          ) {
+            return 'vendor-mui';
+          }
+          if (id.includes('/node_modules/react-icons/')) {
+            return 'vendor-icons';
+          }
+          if (id.includes('/node_modules/framer-motion/')) {
+            return 'vendor-motion';
+          }
+          if (
+            id.includes('/node_modules/slick-carousel/') ||
+            id.includes('/node_modules/react-slick/')
+          ) {
+            return 'vendor-slick';
+          }
+          if (
+            id.includes('/node_modules/@fortawesome/') ||
+            id.includes('/node_modules/@paypal/') ||
+            id.includes('/node_modules/emailjs-com/')
+          ) {
+            return 'vendor-integrations';
+          }
+          if (id.includes('/node_modules/workbox-')) {
+            return 'vendor-workbox';
+          }
+          if (id.includes('/node_modules/lodash/')) {
+            return 'vendor-lodash';
+          }
+          if (id.includes('/node_modules/axios/')) {
+            return 'vendor-axios';
+          }
+          if (id.includes('/node_modules/')) {
+            return 'vendor-misc';
           }
           if (id.includes('components')) {
             return 'components';
